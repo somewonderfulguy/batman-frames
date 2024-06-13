6556db40ea05e7e8bd6cb071243787a6d32dbb04
Dmitriy Yastrebov <somewonderfulguy@gmail.com>
2024-06-13 23:43:23 +0300

    first slide

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,24 @@
-import Image from 'next/image'
+import type { Metadata } from 'next'
+
+const title = 'Batman Frames'
+const description = 'Images of Batman for Warpcast'
+const fcFrameImage = `${process.env.NEXT_PUBLIC_SITE_URL}/batman-00.jpg`
+
+export const metadata: Metadata = {
+  title,
+  description,
+  openGraph: {
+    title,
+    description,
+    images: [fcFrameImage]
+  },
+  other: {
+    'fc:frame': 'vNext',
+    'fc:frame:image': fcFrameImage,
+    'fc:frame:post_url': `${process.env.NEXT_PUBLIC_SITE_URL}/api/batman?id=1`,
+    'fc:frame:button:1': 'Start'
+  }
+}
 
 export default function Home() {
   return <main>Batman frames</main>
