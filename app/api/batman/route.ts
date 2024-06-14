2df5f737dc34f6bf020ffda9bb9940db73180b53
Dmitriy Yastrebov <somewonderfulguy@gmail.com>
2024-06-14 13:30:30 +0300

    rework ids

--- app/api/batman/route.ts
+++ app/api/batman/route.ts
@@ -2,24 +2,26 @@ import { NextRequest, NextResponse } from 'next/server'
 
 export const POST = async (req: NextRequest): Promise<Response> => {
   const searchParams = req.nextUrl.searchParams
-  const _id = searchParams.get('id')
-  const id = _id ? Number(_id) : 0
+  const idOrig = searchParams.get('id')
+  const idNum = idOrig ? Number(idOrig) : 0
+  const idStr = String(idNum).padStart(2, '0')
 
-  console.log(_id, id)
+  const isTheLast = idNum === 4
 
-  const isTheLast = id === 4
-
-  const nextId = isTheLast ? '00' : String(id + 1).padStart(2, '0')
-  const buttonText = isTheLast ? 'Start Over' : 'Next Image' + nextId
-  const image = `${process.env.NEXT_PUBLIC_SITE_URL}/batman-${nextId}.jpg`
+  const buttonText = isTheLast
+    ? 'Start Over'
+    : 'Next Image' + ' ' + (idNum + 1) + ' ' + idOrig
+  const image = `${process.env.NEXT_PUBLIC_SITE_URL}/batman-${idStr}.jpg`
 
   return new NextResponse(`<!DOCTYPE html><html><head>
-      <title>This is frame ${id}</title>
+      <title>This is frame ${idNum}</title>
       <meta property="og:image" content="${image}" />
       <meta property="fc:frame" content="vNext" />
       <meta property="fc:frame:image" content="${image}" />
       <meta property="fc:frame:button:1" content="${buttonText}" />
-      <meta property="fc:frame:post_url" content="${process.env.NEXT_PUBLIC_SITE_URL}/api/batman?id=${id}" />
+      <meta property="fc:frame:post_url" content="${
+        process.env.NEXT_PUBLIC_SITE_URL
+      }/api/batman?id=${idNum + 1}" />
     </head></html>`)
 }
 
